837e172fbdec69dc33408b137941f3903ad62bb8
Mumtaz Ahmad <mumtazahmad2004@gmail.com>
2018-01-24 18:52:37 +0530

    Add files via upload

---
+++ lessons/tab-directive/app.js
@@ -0,0 +1,52 @@
+angular.module('app', [])
+.directive('tab', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: '<div role="tabpanel" ng-show="active" ng-transclude></div>',
+    scope: {
+        heading: '@'
+    },
+    require: '^tabset',
+    link: function(scope, elem, attr, tabsetCtrl) {
+        scope.active = false
+        tabsetCtrl.addTab(scope)
+    },
+    controller: function() {
+        var self = this
+        self.tabs = []
+
+        self.addTab = function addTab(tab) {
+            self.tabs.push(tab);
+            if(self.tabs.length === 1) {
+                tab.active = true
+            }
+        }
+
+        self.select = function(selectedTab) {
+          angular.forEach(self.tabs, function(tab) {
+            if(tab.active && tab !== selectedTab) {
+              tab.active = false;
+            }
+          })
+
+          selectedTab.active = true;
+        }
+    }
+  }
+})
+
+.directive('tabset', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: { },
+    templateUrl: 'tabset.html',
+    bindToController: true,
+    controllerAs: 'tabset',
+    controller: function() {
+      var self = this
+      self.tabs = []
+    }
+  }
+})
